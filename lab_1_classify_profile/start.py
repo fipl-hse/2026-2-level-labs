3142a7e43c18845acb26fdaaeca53efd48331c0e
hamsandwichh <vendetta-1017@ya.ru>
2026-09-10 23:30:11 +0300

    initial mark 4 realization

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -1,6 +1,7 @@
 """
 Language detection starter.
 """
+from main import tokenize, remove_stop_words, calculate_frequencies, get_top_n_words
 
 # pylint: disable=unused-variable, duplicate-code
 
@@ -17,9 +18,15 @@ def main() -> None:
         stopwords = file.read().split("\n")
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
-    result = None
-    assert result, "Detection result is None"
+    #result = None
+    #assert result, "Detection result is None"
 
+    tokenized_de_text = tokenize(de_text)
+    cleaned_de_tokens = remove_stop_words(tokenized_de_text, stopwords)
+    freq_dict = calculate_frequencies(cleaned_de_tokens)
+    top_n_words = get_top_n_words(freq_dict, 7)
 
+    for word in top_n_words:
+        print(word)
 if __name__ == "__main__":
     main()
