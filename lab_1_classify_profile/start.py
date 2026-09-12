3c10164c4aff4b15baf6b1282672478bbea3d148
lcharsk <lida.kashtanova2007@gmail.com>
2026-09-12 20:13:44 +0300

    import function tokenize from main.py to start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,7 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-
+from main import tokenize
 
 def main() -> None:
     """
@@ -20,6 +20,9 @@ def main() -> None:
     result = None
     assert result, "Detection result is None"
 
+    cleaned_de_text = tokenize(de_text)
+    cleaned_unknown_text = tokenize(unknown_text)
+    cleaned_en_text = tokenize(en_text)
 
 if __name__ == "__main__":
     main()
