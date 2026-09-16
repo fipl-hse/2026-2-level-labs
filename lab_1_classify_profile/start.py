fbee03621f93a6cdfa250755c8ad7bb1a668b04f
unknown <nickitavalk@yandex.ru>
2026-09-17 02:04:04 +0300

    improve start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -4,8 +4,8 @@ Language detection starter.
 
 # pylint: disable=unused-variable, duplicate-code
 
-from main import (tokenize, remove_stop_words,
-                  calculate_frequencies, get_top_n_words)
+
+from main import tokenize, remove_stop_words, calculate_frequencies, get_top_n_words
 
 
 def main() -> None:
