2c4e8811eadf216db1ed77e041820ce0d96ac34a
kp9314307@gmail.com <kp9314307@gmail.com>
2026-09-18 12:16:31 +0300

    Fixed tokenize function

--- lab_1_classify_profile/main.py
+++ lab_1_classify_profile/main.py
@@ -26,7 +26,7 @@ def tokenize(text: str) -> Sequence[str] | None:
         Sequence[str] | None: Sequence of lower-cased tokens without punctuation.
         Returns None if input text is not a string.
     """
-     if not isinstance(text, str):
+    if not isinstance(text, str):
         return None
 
     tokens = []
@@ -52,7 +52,7 @@ def remove_stop_words(tokens: Sequence[str], stop_words: Sequence[str]) -> Seque
         Sequence[str] | None: Sequence of tokens without stop words.
         Returns None in case of incorrect input types.
     """
-            if not isinstance(tokens, (list, tuple)):
+    if not isinstance(tokens, (list, tuple)):
         return None
     if not all(isinstance(token, str) for token in tokens):
         return None
