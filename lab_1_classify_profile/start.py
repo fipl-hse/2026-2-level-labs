e717de0b0fe16cdc4b0f33b6ec869413f650a98c
sonikulagija <gosofakula007@gmail.com>
2026-09-20 15:57:35 +0300

    Работа со стилем

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,30 +3,41 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-from main import tokenize, remove_stop_words, calculate_frequencies, get_top_n_words
+from main import (
+    tokenize,
+    remove_stop_words,
+    calculate_frequencies,
+    get_top_n_words
+)
+
 
 def main() -> None:
     """
     Launches an implementation.
     """
-    with open("lab_1_classify_profile/assets/texts/de.txt", "r", encoding="utf-8") as file:
+    with open(
+        "lab_1_classify_profile/assets/texts/de.txt", "r", encoding="utf-8"
+    ) as file:
         de_text = file.read()
-    with open("lab_1_classify_profile/assets/texts/unknown.txt", "r", encoding="utf-8") as file:
+    with open(
+        "lab_1_classify_profile/assets/texts/unknown.txt", "r", encoding="utf-8"
+    ) as file:
         unknown_text = file.read()
-    with open("lab_1_classify_profile/assets/stopwords.txt", "r", encoding="utf-8") as file:
+    with open(
+        "lab_1_classify_profile/assets/stopwords.txt", "r", encoding="utf-8"
+    ) as file:
         stopwords = file.read().split("\n")
-    with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
+    with open(
+        "lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8"
+    ) as file:
         en_text = file.read()
     de_tokens = tokenize(de_text)
     de_wostopwords = remove_stop_words(de_tokens, stopwords)
     de_freq = calculate_frequencies(de_wostopwords)
-    result = get_top_n_words (de_freq, 7)
-    print (result)
+    result = get_top_n_words(de_freq, 7)
+    print(result)
     assert result, "Detection result is None"
 
 
 if __name__ == "__main__":
     main()
-
-
-
