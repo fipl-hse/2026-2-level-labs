10bd0cfc35145dc840193156d5bed0f755d83d9e
lcharsk <lida.kashtanova2007@gmail.com>
2026-09-20 15:26:57 +0300

    modified import in start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,14 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-from main import (calculate_frequencies, get_top_n_words, remove_stop_words, tokenize, create_language_profile, detect_language_by_top_n)
+from main import (
+    calculate_frequencies,
+    create_language_profile,
+    detect_language_by_top_n,
+    get_top_n_words,
+    remove_stop_words,
+    tokenize,
+)
 
 
 def main() -> None:
