909fe456f0cc02a6b92113e6c764e76e6a3b2c0c
sonikulagija <gosofakula007@gmail.com>
2026-09-24 22:46:30 +0300

    working on start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,7 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-from main import (
+from lab_1_classify_profile.main import (
     calculate_frequencies,
     compare_profiles_by_top_n,
     create_language_profile,
