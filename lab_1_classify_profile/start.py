312305fcba71c8e581450020f373475d6248c17a
Таня <tanya.shereshevskaya@gmail.com>
2026-09-24 23:36:28 +0300

    functions for mark 6 complete

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,14 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-
+from lab_1_classify_profile.main import (
+    calculate_frequencies,
+    create_language_profile,
+    detect_language_by_top_n,
+    get_top_n_words,
+    remove_stop_words,
+    tokenize,
+)
 
 def main() -> None:
     """
@@ -18,6 +25,28 @@ def main() -> None:
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
     result = None
+    tokens = tokenize(de_text)
+
+    if tokens is not None:
+        cleaned_tokens = remove_stop_words(tokens, stopwords)
+
+    if cleaned_tokens is not None:
+        freq_dict = calculate_frequencies(cleaned_tokens)
+
+    if freq_dict is not None:
+        result = get_top_n_words(freq_dict, 7)
+
+    print("Топ 7 популярных слов (нем):", result)
+
+    de_profile = create_language_profile("de", de_text, stopwords)
+    en_profile = create_language_profile("en", en_text, stopwords)
+    unknown_profile = create_language_profile("unknown", unknown_text, stopwords)
+
+    if de_profile is not None and en_profile is not None and unknown_profile is not None:
+        result = detect_language_by_top_n(unknown_profile, en_profile, de_profile, 15)
+
+    print("Язык текста:", result)
+
     assert result, "Detection result is None"
 
 
