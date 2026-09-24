d86ef914b7b4602329526b3fef68e67aa4d7a945
lcharsk <lida.kashtanova2007@gmail.com>
2026-09-24 23:26:29 +0300

    make import from main

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,7 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-from main import (
+from lab_1_classify_profile.main import (
     calculate_frequencies,
     create_language_profile,
     detect_language_by_mse,
