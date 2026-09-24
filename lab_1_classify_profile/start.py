5a57a2fed0ec90d982c67698f5b8f2dd0c2455af
unknown <nickitavalk@yandex.ru>
2026-09-24 16:21:41 +0300

    improve import style #2

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -4,7 +4,8 @@ Language detection starter.
 
 # pylint: disable=unused-variable, duplicate-code
 
-from lab_1_classify_profile.main import detect_language_by_mse, create_language_profile
+from lab_1_classify_profile.main import (
+    create_language_profile, detect_language_by_mse)
 
 
 def main() -> None:
