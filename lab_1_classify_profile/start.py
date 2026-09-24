b0087f614cd838f01f59f328d6a5a634d29cbd3f
unknown <nickitavalk@yandex.ru>
2026-09-24 13:16:22 +0300

    improve start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -14,6 +14,9 @@ def main() -> None:
         get_top_n_words,
         remove_stop_words,
         tokenize,
+        create_language_profile,
+        check_profile,
+        compare_profiles_by_top_n
     )
 
     with open("lab_1_classify_profile/assets/texts/de.txt", "r", encoding="utf-8") as file:
@@ -25,10 +28,10 @@ def main() -> None:
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
 
-    tokens = tokenize(de_text)
-    cleared_tokens = remove_stop_words(tokens, stopwords)
-    freq_dict = calculate_frequencies(cleared_tokens)
-    result = get_top_n_words(freq_dict, 7)
+    profile_de = create_language_profile("de", de_text, stopwords)
+    profile_en = create_language_profile("en", en_text, stopwords)
+    unknown_profile = create_language_profile("en", unknown_text, stopwords)
+    result = compare_profiles_by_top_n(unknown_profile, profile_en, profile_de)
     assert result, "Detection result is None"
 
 
