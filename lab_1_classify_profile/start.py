d05864d358b64d0094cc1e44990b911635f04311
Таня <tanya.shereshevskaya@gmail.com>
2026-09-25 00:23:26 +0300

    fixed line indent

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,6 +3,7 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
+
 from lab_1_classify_profile.main import (
     calculate_frequencies,
     create_language_profile,
@@ -11,6 +12,7 @@ from lab_1_classify_profile.main import (
     remove_stop_words,
     tokenize,
 )
+
 
 def main() -> None:
     """
@@ -46,8 +48,6 @@ def main() -> None:
         result = detect_language_by_top_n(unknown_profile, en_profile, de_profile, 15)
 
     print("Язык текста:", result)
-
-
 
     assert result, "Detection result is None"
 
