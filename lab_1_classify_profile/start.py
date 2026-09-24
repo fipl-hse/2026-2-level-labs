99f7e9b58d23eccfb02ae7b1b54b71ec9c6bf81b
unknown <nickitavalk@yandex.ru>
2026-09-24 16:44:41 +0300

    improve start.py part 5

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -2,9 +2,11 @@
 Language detection starter.
 """
 
-
-from .main import (
-    create_language_profile, detect_language_by_mse)
+# pylint: disable=unused-variable, duplicate-code
+from lab_1_classify_profile.main import (
+    create_language_profile,
+    detect_language_by_mse,
+)
 
 
 def main() -> None:
