58fa0672f88c09bd01334e6bfd44b5c6ad1508ea
hamsandwichh <vendetta-1017@ya.ru>
2026-09-25 14:55:00 +0300

    tried fixing import style check (by importing in alph order)

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -2,8 +2,8 @@
 Language detection starter.
 """
 from main import (
-    tokenize, remove_stop_words, calculate_frequencies, get_top_n_words,
-    create_language_profile, detect_language_by_top_n
+    calculate_frequencies, create_language_profile, detect_language_by_top_n,
+    get_top_n_words, remove_stop_words, tokenize
 )
 
 # pylint: disable=unused-variable, duplicate-code
