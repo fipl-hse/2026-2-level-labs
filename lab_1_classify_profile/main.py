081a9eed55b5d1b89ed3f22d3557627bb9a1095c
rishhaa <apbelyaeva@edu.hse.ru>
2026-09-26 17:12:09 +0300

    fix: add missing blank lines after imports

--- lab_1_classify_profile/main.py
+++ lab_1_classify_profile/main.py
@@ -6,6 +6,7 @@ Language detection
 
 # pylint:disable=unused-argument
 from typing import Sequence
+
 FreqDictType = dict[str, float]
 "Frequency dictionary. Contains pairs of token and its frequency."
 ProfileType = tuple[str, FreqDictType, int]
