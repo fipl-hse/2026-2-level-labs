feca5001b8ba5cc20cd8855263d33e2fd0fe3894
rishhaa <apbelyaeva@edu.hse.ru>
2026-09-26 17:24:59 +0300

    fixed

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -7,6 +7,8 @@ Language detection starter.
 
 from lab_1_classify_profile.main import create_language_profile, detect_language_by_mse
 
+
+
 def main() -> None:
     """
     Launches an implementation.
