0728c56ea19cb58d276eaaa2918c80fc7b1822dc
rishhaa <apbelyaeva@edu.hse.ru>
2026-09-26 17:18:51 +0300

    fixed

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,6 +3,8 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
+
+
 from lab_1_classify_profile.main import create_language_profile, detect_language_by_mse
 
 def main() -> None:
