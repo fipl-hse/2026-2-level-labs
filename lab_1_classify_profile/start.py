c0e8ae442fabea1d81d6f27b71059aa6602adfe9
unknown <nickitavalk@yandex.ru>
2026-09-26 18:13:07 +0300

    improve style for 10 part 4

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,6 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-
 from lab_1_classify_profile.main import (
     create_language_profile,
     detect_language_advanced,
