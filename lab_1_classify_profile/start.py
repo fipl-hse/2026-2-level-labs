8d64de92eab4f6acab7575c0ec01fbae25e4f885
Veronika Ignatenko <niksonflamingson@gmail.com>
2026-09-26 11:13:48 +0300

    LAB1: fixed start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -29,20 +29,24 @@ def main() -> None:
         en_text = file.read()
 
     tokens = tokenize(de_text)
+    assert tokens is not None
     filtered = remove_stop_words(tokens, stopwords)
+    assert filtered is not None
     freq_dict = calculate_frequencies(filtered)
+    assert freq_dict is not None
     print(get_top_n_words(freq_dict, 7))
 
     de_profile = create_language_profile('de', de_text, stopwords)
     en_profile = create_language_profile('en', en_text, stopwords)
     unknown_profile = create_language_profile('unknown', unknown_text, stopwords)
+    assert de_profile is not None
+    assert en_profile is not None
+    assert unknown_profile is not None
 
     result = detect_language_by_top_n(unknown_profile, en_profile, de_profile, 7)
     print("Detected language:", result)
     result_mse = detect_language_by_mse(unknown_profile, en_profile, de_profile)
     print("Detected language by MSE:", result_mse)
-
-    assert result, "Detection result is None"
 
 
 if __name__ == "__main__":
